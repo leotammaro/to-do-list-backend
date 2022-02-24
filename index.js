9676176b83518371863f7f9588dd52991bb24d2d
Leonel Tammaro <tammaroleonel@gmail.com>
2022-02-24 14:45:01 -0300

    fix credential admin

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const { verifyToken } = require("./middlewares/verifyToken");
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
 
