425ffc912cf5523d8f6fba9855019eb42e54f5ea
Leonel Tammaro <tammaroleonel@gmail.com>
2022-10-25 20:01:31 -0300

    change value private key

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { verifyToken } = require("./middlewares/verifyToken");
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
